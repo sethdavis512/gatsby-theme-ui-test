f83131dd56744bdf3f3b8af63f4924a9fd397bbe
Seth Davis <sethdavis512@gmail.com>
2021-04-10 22:13:20 -0500

    Add encode and submit functions

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,18 @@
 import React, { useState } from 'react';
 import { Box, Input, Label } from 'theme-ui';
 
+const Constants = {
+    FORM_NAME: 'contact'
+};
+
+const encode = data => {
+    return Object.keys(data)
+        .map(
+            key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
+        )
+        .join('&');
+};
+
 const useForm = initialValues => {
     const [formState, setFormState] = useState(initialValues);
 
@@ -20,16 +32,33 @@ const IndexPage = () => {
         email: ''
     });
 
+    const handleFormSubmit = event => {
+        event.preventDefault();
+
+        fetch('/', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: encode({ 'form-name': Constants.FORM_NAME, ...formState })
+        })
+            .then(() => alert('Success!'))
+            .catch(error => alert(error));
+    };
+
     return (
         <main>
             <form
-                name="contact"
+                name={Constants.FORM_NAME}
                 method="post"
                 data-netlify="true"
                 data-netlify-honeypot="bot-field"
+                onSubmit={handleFormSubmit}
             >
                 {/* You still need to add the hidden input with the form name to your JSX form */}
-                <input type="hidden" name="my-form-name" value="contact" />
+                <input
+                    type="hidden"
+                    name="my-form-name"
+                    value={Constants.FORM_NAME}
+                />
                 <Box sx={{ mb: 4 }}>
                     <Label htmlFor="firstName">First Name</Label>
                     <Input

4465fe6ee8c4fd96065e139ee46704793ec402d1
Seth Davis <sethdavis512@gmail.com>
2021-04-11 00:59:37 -0500

    Add get to know you form

--- src/pages/index.js
+++ src/pages/index.js
@@ -53,58 +53,105 @@ const ErrorText = ({ children }) => (
     <Text sx={{ color: 'red' }}>{children}</Text>
 );
 
+const ContactUs = () => (
+    <Formik
+        initialValues={{
+            firstName: '',
+            lastName: '',
+            email: '',
+            message: ''
+        }}
+        validationSchema={ContactSchema}
+        onSubmit={(values, actions) => {
+            fetch('/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: encode({
+                    'form-name': Constants.NETLIFY_FORM_NAME,
+                    ...values
+                })
+            })
+                .then(() => {
+                    actions.resetForm();
+                })
+                .catch(error => alert(error));
+        }}
+    >
+        <Form
+            name={Constants.NETLIFY_FORM_NAME}
+            method="post"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+        >
+            {/* You still need to add the hidden input with the form name to your JSX form */}
+            <Field
+                type="hidden"
+                name="my-form-name"
+                value={Constants.NETLIFY_FORM_NAME}
+            />
+            <ComposedField label="First Name" name="firstName" />
+            <ComposedField label="Last Name" name="lastName" />
+            <ComposedField label="Email" name="email" type="email" />
+            <ComposedField
+                as={Textarea}
+                helpText="Give us your honest thoughts"
+                label="Message"
+                name="message"
+            />
+            <Button type="submit">Contact Me!</Button>
+        </Form>
+    </Formik>
+);
+
+const GetToKnowYou = () => (
+    <Formik
+        initialValues={{
+            favoriteColor: '',
+            favoritePizza: ''
+        }}
+        onSubmit={(values, actions) => {
+            fetch('/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: encode({
+                    'form-name': 'get-to-know-you',
+                    ...values
+                })
+            })
+                .then(() => {
+                    actions.resetForm();
+                })
+                .catch(error => alert(error));
+        }}
+    >
+        <Form
+            name={'get-to-know-you'}
+            method="post"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+        >
+            {/* You still need to add the hidden input with the form name to your JSX form */}
+            <Field
+                type="hidden"
+                name="get-to-know-you"
+                value="get-to-know-you"
+            />
+            <ComposedField label="Favorite Color" name="favoriteColor" />
+            <ComposedField label="Favorite Pizza" name="favoritePizza" />
+            <Button type="submit">My faves!</Button>
+        </Form>
+    </Formik>
+);
+
 const IndexPage = () => {
     return (
         <Box as="main" sx={{ maxWidth: '600px', mx: 'auto', my: 4 }}>
-            <Formik
-                initialValues={{
-                    firstName: '',
-                    lastName: '',
-                    email: '',
-                    message: ''
-                }}
-                validationSchema={ContactSchema}
-                onSubmit={(values, actions) => {
-                    fetch('/', {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/x-www-form-urlencoded'
-                        },
-                        body: encode({
-                            'form-name': Constants.NETLIFY_FORM_NAME,
-                            ...values
-                        })
-                    })
-                        .then(() => {
-                            actions.resetForm();
-                        })
-                        .catch(error => alert(error));
-                }}
-            >
-                <Form
-                    name={Constants.NETLIFY_FORM_NAME}
-                    method="post"
-                    data-netlify="true"
-                    data-netlify-honeypot="bot-field"
-                >
-                    {/* You still need to add the hidden input with the form name to your JSX form */}
-                    <Field
-                        type="hidden"
-                        name="my-form-name"
-                        value={Constants.NETLIFY_FORM_NAME}
-                    />
-                    <ComposedField label="First Name" name="firstName" />
-                    <ComposedField label="Last Name" name="lastName" />
-                    <ComposedField label="Email" name="email" type="email" />
-                    <ComposedField
-                        as={Textarea}
-                        helpText="Give us your honest thoughts"
-                        label="Message"
-                        name="message"
-                    />
-                    <Button type="submit">Contact Me!</Button>
-                </Form>
-            </Formik>
+            <ContactUs />
+            <GetToKnowYou />
         </Box>
     );
 };
